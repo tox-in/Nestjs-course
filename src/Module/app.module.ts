97b444fc713b0977125040a57a74ba96679a5c12
tox-in <habimanat78@gmail.com>
2024-12-14 11:10:53 +0200

    module changes

--- src/Module/app.module.ts
+++ src/Module/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from '../Controller/app.controller';
 import { AppService } from '../Services/app.service';
 import { TodoModule } from './todo.module';
+import { TodoController } from 'src/Controller/todo.controller';
+import { TodoService } from 'src/Services/todo.service';
 
 @Module({
   imports: [TodoModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [TodoController],
+  providers: [AppService, TodoService],
 })
 export class AppModule {}
